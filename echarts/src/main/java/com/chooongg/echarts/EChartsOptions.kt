3a9e19c0a4742fca9f34a93c234662a11810daef
Chooongg <chooongg@outlook.com>
2021-12-24 17:57:57 +0800

    .

--- echarts/src/main/java/com/chooongg/echarts/EChartsOptions.kt
+++ echarts/src/main/java/com/chooongg/echarts/EChartsOptions.kt
@@ -1,45 +1,47 @@
 package com.chooongg.echarts
 
-data class EChartsOptions(
-    private var title: Any? = null,
-    private var legend: Any? = null,
-    private var grid: Any? = null,
-    private var xAxis: Any? = null,
-    private var yAxis: Any? = null,
-    private var polar: Any? = null,
-    private var radiusAxis: Any? = null,
-    private var angleAxis: Any? = null,
-    private var radar: Any? = null,
-    private var dataZoom: Any? = null,
-    private var visualMap: Any? = null,
-    private var tooltip: Any? = null,
-    private var axisPointer: Any? = null,
-    private var toolbox: Any? = null,
-    private var brush: Any? = null,
-    private var geo: Any? = null,
-    private var parallel: Any? = null,
-    private var singleAxis: Any? = null,
-    private var timeLine: Any? = null,
-    private var graphic: Any? = null,
-    private var calendar: Any? = null,
-    private var dataset: Any? = null,
-    private var aria: Any? = null,
-    private var series: Any? = null,
-    private var darkMode: Boolean? = null,
-    private var backgroundColor: Any? = null,
-    private var textStyle: Any? = null,
-    private var animation: Any? = null,
-    private var animationThreshold: Any? = null,
-    private var animationDuration: Any? = null,
-    private var animationEasing: Any? = null,
-    private var animationDelay: Any? = null,
-    private var animationDurationUpdate: Any? = null,
-    private var animationEasingUpdate: Any? = null,
-    private var animationDelayUpdate: Any? = null,
-    private var stateAnimation: Any? = null,
-    private var blendMode: Any? = null,
-    private var hoverLayerThreshold: Any? = null,
-    private var useUTC: Any? = null,
-    private var options: Any? = null,
-    private var media: Any? = null,
-)
+class EChartOptions {
+    private var title: Any? = null
+    private var legend: Any? = null
+    private var grid: Any? = null
+    private var xAxis: Any? = null
+    private var yAxis: Any? = null
+    private var polar: Any? = null
+    private var radiusAxis: Any? = null
+    private var angleAxis: Any? = null
+    private var radar: Any? = null
+    private var dataZoom: Any? = null
+    private var visualMap: Any? = null
+    private var tooltip: Any? = null
+    private var axisPointer: Any? = null
+    private var toolbox: Any? = null
+    private var brush: Any? = null
+    private var geo: Any? = null
+    private var parallel: Any? = null
+    private var singleAxis: Any? = null
+    private var timeLine: Any? = null
+    private var graphic: Any? = null
+    private var calendar: Any? = null
+    private var dataset: Any? = null
+    private var aria: Any? = null
+    private var series: Any? = null
+    private var darkMode: Boolean? = null
+    private var backgroundColor: Any? = null
+    private var textStyle: Any? = null
+    private var animation: Any? = null
+    private var animationThreshold: Any? = null
+    private var animationDuration: Any? = null
+    private var animationEasing: Any? = null
+    private var animationDelay: Any? = null
+    private var animationDurationUpdate: Any? = null
+    private var animationEasingUpdate: Any? = null
+    private var animationDelayUpdate: Any? = null
+    private var stateAnimation: Any? = null
+    private var blendMode: Any? = null
+    private var hoverLayerThreshold: Any? = null
+    private var useUTC: Any? = null
+    private var options: Any? = null
+    private var media: Any? = null
+
+
+}
